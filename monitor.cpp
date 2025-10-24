0c6254ba66fa35d829fbc7c7177aeea9a10c1c04
Vajra <VajraJagadish.Shetty@in.bosch.com>
2025-10-24 10:57:03 +0530

    Refactor blinking function for vitals check

--- monitor.cpp
+++ monitor.cpp
@@ -3,29 +3,31 @@
 #include <thread>
 #include <chrono>
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-    auto blink = []() {
-        for (int i = 0; i < 6; ++i) {
-            std::cout << "\r* " << std::flush;
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            std::cout << "\r *" << std::flush;
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-    };
+// Reusable blinking function
+void blinkWarning() {
+    for (int i = 0; i < 6; ++i) {
+        std::cout << "\r* " << std::flush;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "\r *" << std::flush;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
 
+// Main vitals check
+int vitalsOk(float temperature, float pulseRate, float spo2) {
     if (temperature > 102.0f || temperature < 95.0f) {
         std::cout << "Temperature is critical!\n";
-        blink();
+        blinkWarning();
         return 0;
     }
     if (pulseRate < 60.0f || pulseRate > 100.0f) {
         std::cout << "Pulse Rate is out of range!\n";
-        blink();
+        blinkWarning();
         return 0;
     }
     if (spo2 < 90.0f) {
         std::cout << "Oxygen Saturation out of range!\n";
-        blink();
+        blinkWarning();
         return 0;
     }
     return 1;
