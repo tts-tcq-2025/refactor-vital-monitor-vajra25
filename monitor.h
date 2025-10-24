62a88e8fb740da2fbd531136af492410f71e6fd8
Vajra <VajraJagadish.Shetty@in.bosch.com>
2025-10-24 10:44:13 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,7 +1,3 @@
 #pragma once
 
-// Check if vitals are in range (pure function)
-bool vitalsOk(float temperature, float pulseRate, float spo2);
-
-// Print warning message with blinking animation (I/O function)
-void printVitalWarning(const char* message, int flashes = 6);
+int vitalsOk(float temperature, float pulseRate, float spo2);
