55ff8ea99d6a0d38962cae373787b4b4dd6b9e38
Vajra <VajraJagadish.Shetty@in.bosch.com>
2025-10-24 10:44:28 +0530

    Refactor vitalsOk to add blinking warning feature
    Refactor vitalsOk function to include blinking warning for critical vitals.

--- monitor.cpp
+++ monitor.cpp
@@ -3,21 +3,30 @@
 #include <thread>
 #include <chrono>
 
-// Pure function: check if vitals are OK
-bool vitalsOk(float temperature, float pulseRate, float spo2) {
-    if (temperature < 95.0f || temperature > 102.0f) return false;
-    if (pulseRate < 60.0f || pulseRate > 100.0f) return false;
-    if (spo2 < 90.0f) return false;
-    return true;
-}
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+    auto blink = []() {
+        for (int i = 0; i < 6; ++i) {
+            std::cout << "\r* " << std::flush;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::cout << "\r *" << std::flush;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    };
 
-// I/O function: print blinking warning
-void printVitalWarning(const char* message, int flashes) {
-    std::cout << message << "\n";
-    for (int i = 0; i < flashes; ++i) {
-        std::cout << "\r* " << std::flush;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "\r *" << std::flush;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    if (temperature > 102.0f || temperature < 95.0f) {
+        std::cout << "Temperature is critical!\n";
+        blink();
+        return 0;
     }
+    if (pulseRate < 60.0f || pulseRate > 100.0f) {
+        std::cout << "Pulse Rate is out of range!\n";
+        blink();
+        return 0;
+    }
+    if (spo2 < 90.0f) {
+        std::cout << "Oxygen Saturation out of range!\n";
+        blink();
+        return 0;
+    }
+    return 1;
 }
